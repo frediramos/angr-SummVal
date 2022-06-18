ba2f7d9ab6906800c5e208d1e92dae6f00fca460
Frederico Ramos <frediramos@gmail.com>
2022-06-18 10:06:21 +0100

    Convert function call addrs to symbols (fnames)

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
+from lib2to3.pytree import convert
 import subprocess as sp
 import json
 import sys
 import os
+import re
 
 from config import get_config, get_stats, set_stats, get_SimManager
 from macros import RESULTS_DIR, BIN_NAME, BIN_PATH, TIMEOUT, STATS
@@ -13,18 +15,50 @@ def get_num_of_paths():
 
 
 def get_fnames():
+
+	def clean_addr(addr):
+		if addr[0] == '0':
+			addr = addr[1:]
+		return addr
+
+	symbols = {}
 	bin_path, = get_config(BIN_PATH)
-	
+
+	# nm binary | grep \ T \ 
 	command_nm = ['nm', bin_path ]
 	command_grep = ['grep', '\ T\ ']
 	
 	p1 = sp.Popen(command_nm, stdout=sp.PIPE)
 	p2 = sp.Popen(command_grep, stdin=p1.stdout, stdout=sp.PIPE)
-	out, _ = p2.communicate()
-	out = out.decode('utf-8')
-	print(out)
-
+	nm_out, _ = p2.communicate()
+	nm_out = nm_out.decode('utf-8')
 	
+	for line in nm_out.splitlines():
+		split = line.split()
+		addr = split[0]
+		symbol = split[2]
+		symbols[clean_addr(addr)] = symbol
+
+
+	# objdump -d binary | grep \ plt \
+	command_objdump = ['objdump', '-d', bin_path]
+	command_grep = ['grep', '@plt\>']
+
+	p3 = sp.Popen(command_objdump, stdout=sp.PIPE)
+	p4 = sp.Popen(command_grep, stdin=p3.stdout, stdout=sp.PIPE)
+	objdump_out, _ = p4.communicate()
+	objdump_out = objdump_out.decode('utf-8')	
+
+	for line in objdump_out.splitlines():
+		split = line.split()
+		addr = split[-2]
+		symbol = split[-1]
+		symbol = re.split(r'[@ \< \>]', symbol)[1]		
+		symbols[clean_addr(addr)] = symbol 
+
+	return symbols
+
+
 
 
 def timout_handler(signum, frame):
@@ -40,7 +74,8 @@ def count_fcall(state):
 	addr = str(state.inspect.function_address) 	#<BV32 0x80483a3>	
 	addr = addr.split()[1] 			 		 	#0x80483a3>
 	addr = addr[:-1]					  		#0x80483a3
-	
+	addr = addr[2:]								#80483a3
+
 	f_called, = get_stats(F_CALLED)
 	if addr in f_called.keys():
 		f_called[addr] += 1
@@ -72,8 +107,15 @@ def save_stats(is_timeout=False):
 
 	out_stats['N_Paths'] = get_num_of_paths()
 	
-	get_fnames()  
-	out_stats['Fcalled'] = f_called
+	#Convert function call addrs to symbols
+	fnames = get_fnames()
+	converted = {}
+	for f in f_called.keys():
+		if f in fnames.keys():
+			fname = fnames[f]
+			converted[fname] = f_called[f]
+
+	out_stats['Fcalled'] = converted
 	out_stats['Fcalled'].pop('main', None)
 	
 	out_stats = {name:out_stats}
