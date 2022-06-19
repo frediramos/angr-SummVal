acabfe9cd00a85440a38c437cb266551808c5e48
Frederico Ramos <frediramos@gmail.com>
2022-06-19 20:44:30 +0100

    sligthly change dir structure

--- Summaries/CaseStudies.py
+++ Summaries/CaseStudies.py
@@ -1,8 +1,18 @@
 from angr import SimProcedure
 
+class Assert(SimProcedure):
+
+    def run(self, value):
+        value = self.state.solver.eval(value) 
+        if value == 0:
+            self.exit()
+        else:
+            self.ret()
+
+
+
 HASH_COUNTER = 0
 class hashmap_hash(SimProcedure):
-
 
     def run(self, strkey, maxHash):
         maxHash = self.state.solver.eval(maxHash)     
